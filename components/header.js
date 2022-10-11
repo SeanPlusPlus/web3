3e768324fd31b187ce54bf402dce264e08035e1f
Sean Stephenson <sean.m.stephenson@disney.com>
2022-10-11 08:38:59 -0700

    main: header update

--- components/header.js
+++ components/header.js
@@ -9,17 +9,17 @@ const Header = () => {
       <link rel="icon" href="https://i.imgur.com/hH8h8aM.png" />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://web3.seanplusplus.vercel.app/" />
+      <meta property="og:url" content="https://web3-seanplusplus.vercel.app/" />
       <meta property="og:title" content="Menu XYZ" />
       <meta property="og:description" content="Web3 Sean++" />
       <meta property="og:image" content="https://i.imgur.com/hH8h8aM.png" />
       
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:domain" value="web3.seanplusplus.vercel.app" />
+      <meta name="twitter:domain" value="web3-seanplusplus.vercel.app" />
       <meta name="twitter:title" value="Web3 Sean++" />
       <meta name="twitter:description" value="Web Sean++" />
       <meta name="twitter:image" content="https://i.imgur.com/hH8h8aM.png" />
-      <meta name="twitter:url" value="https://web3.seanplusplus.vercel.app/" />
+      <meta name="twitter:url" value="https://web3-seanplusplus.vercel.app/" />
     </Head>
   )
 }
