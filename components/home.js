de0aedf65d1f82d45202883e4e4ad274941a50dd
Sean Stephenson <sean.m.stephenson@disney.com>
2022-10-24 15:48:40 -0700

    main: axios

--- components/home.js
+++ components/home.js
@@ -1,6 +1,18 @@
 import Image from "next/image"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 const Home = () => {
+
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    const { data } = await axios.get(`https://simplejson.vercel.app/api/CvuceSWn`);
+    setData(data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <div className="hero bg-base-200 min-h-screen">
@@ -12,6 +24,7 @@ const Home = () => {
             <p className="py-6">
               A collection of scripts and templates to kickoff a web3 project
             </p>
+            <div>{JSON.stringify(data)}</div>
           </div>
         </div>
       </div>
